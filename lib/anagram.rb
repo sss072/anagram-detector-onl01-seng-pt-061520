5044d6962d59351468d03cc57e04f2204637f624
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-31 15:42:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end
   def match(array)
     array.select do |element|
-      element.split.sort == @word.split.sort
+      element.split("").sort == @word.split("").sort
     end
   end
 end
